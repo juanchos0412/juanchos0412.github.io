551cd49cebe9748e875c749fad86a19eacc19762
juanchos0412 <valderramadienasjuansebastian@gmail.com>
2024-08-01 00:38:31 -0500

    quitar base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juanchos0412.github.io/',
-  base: 'juanchos0412.github.io',
-})
+  site: "https:/./",
+});
